ef1f9d525ff58b2ef9f1cef94e81b5a60fbab4a5
Michael Minter <mothore@gmail.com>
2014-09-14 18:22:55 -0500

    add total_data_usage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def total_data_usage(bytes)
+    case bytes.to_i
+      when 1000000000
+        return "#{(bytes.to_i / 1000000000).round(3)} GB"
+      when 1000000..999999999
+        return "#{(bytes.to_i / 1000000).round(3)} MB"
+      when 1000..999999
+        return "#{(bytes.to_i / 1000).round(3)} KB"
+      when 0..999
+        return "#{bytes} B"
+    end
+  end
 end
