44112e44d5a5cba28e224effd7a2655e390e44ed
Michael Minter <mothore@gmail.com>
2014-09-15 21:21:47 -0500

    add gray portlet if last request was more than 30 minutes ago

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,30 +1,13 @@
 module ServersHelper
-  def panel_icon(type)
-    case type.to_i
-      when 3
-        return '<em class="fa fa-gear fa-fw"></em>'.html_safe # process
-      when 5
-        return '<em class="fa fa-hdd-o fa-fw"></em>'.html_safe # system
-      else
-        return '<em class="fa fa-hdd-o fa-fw"></em>'.html_safe
-    end
-  end
-
-  def panel_color(status)
-    if status.to_i == 0
-      return 'success'
+  def panel_color(current_event)
+    if current_event.created_at < 30.minutes.ago
+      return 'gray'
     else
-      return 'danger'
-    end
-  end
-
-  def panel_content(event)
-    case event.type.to_i
-      when 3
-        return [event.memory, event.cpu]
-      when 5
-        return event.system
+      if current_event.status.to_i == 0
+        return 'success'
       else
+        return 'danger'
+      end
     end
   end
 
