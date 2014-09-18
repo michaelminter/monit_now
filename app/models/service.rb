37dbc517d5856992324b882f7c098a044b1d47d3
Michael Minter <mothore@gmail.com>
2014-09-18 18:59:49 -0500

    limit new row order

--- app/models/service.rb
+++ app/models/service.rb
@@ -7,7 +7,7 @@ class Service
   field :name,         type: String
   field :type,         type: Integer
   field :order_column, type: Integer, default: rand(1..3)
-  field :order_row,    type: Integer, default: rand(0..99)
+  field :order_row,    type: Integer, default: rand(50..99)
 
   belongs_to :server
   has_many :events
