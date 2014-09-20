4e39bf47173012ab4a7c0cc4cf492d87798320c8
Michael Minter <mothore@gmail.com>
2014-09-20 16:46:31 -0500

    fix where new portlets go

--- app/models/portlet.rb
+++ app/models/portlet.rb
@@ -6,7 +6,7 @@ class Portlet
   field :server_id,    type: BSON::ObjectId
   field :name,         type: String
   field :type,         type: Integer
-  field :order_column, type: Integer, default: rand(1..3)
+  field :order_column, type: Integer, default: rand(1..2)
   field :order_row,    type: Integer, default: rand(50..99)
 
   belongs_to :server
