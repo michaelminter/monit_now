b0c312a2d63374e44bef169dfe75e97c35b40a88
Michael Minter <mothore@gmail.com>
2014-09-20 13:51:27 -0500

    update events table

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = Server.find(params[:server_id]).events
+    @events = Server.find(params[:server_id]).events.order_by(:created_at.desc)
   end
 end
