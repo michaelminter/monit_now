3e5132eea1ec8ed6aca9d976b8f66d54253a5844
Michael Minter <mothore@gmail.com>
2014-09-24 16:41:25 -0500

    create account type seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+AccountType.create({
+    data_limit: 100000000,
+    name: 'Basic - 100 MB',
+    price: 9.99,
+    stripe_subscription_plan: 'basic-100MB-9-99'
+                   })
+AccountType.create({
+     data_limit: 500000000,
+     name: 'Standard - 500 MB',
+     price: 39.99,
+     stripe_subscription_plan: 'standard-500MB-39-99'
+                   })
+AccountType.create({
+     data_limit: 100000000000,
+     name: 'Advance - 1 GB',
+     price: 79.99,
+     stripe_subscription_plan: 'advance-1GB-79-99'
+                   })
+AccountType.create({
+     data_limit: 200000000000,
+     name: 'Professional - 2 GB',
+     price: 179.99,
+     stripe_subscription_plan: 'professional-2GB-179-99'
+                   })
