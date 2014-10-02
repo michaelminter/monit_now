535adfee3ce3cf9e14ec094690c7d1c11bbb59d8
Michael Minter <mothore@gmail.com>
2014-10-02 09:55:00 -0500

    fucking dummy didnt include confirmable action correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   ## Database authenticatable
   field :email,              type: String, default: ''
