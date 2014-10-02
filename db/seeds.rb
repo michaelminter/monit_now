bd67faa92a0d05452be9ca3a67d69f678dab36ee
Michael Minter <mothore@gmail.com>
2014-10-02 00:07:24 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AccountType.create({
     data_limit: 100000000,
-    name: 'Basic - 100 MB',
+    name: 'basic',
+    label: 'Basic',
+    order: 0,
     price: 9.99,
     stripe_subscription_plan: 'basic-100MB-9-99'
                    })
 AccountType.create({
      data_limit: 500000000,
-     name: 'Standard - 500 MB',
+     name: 'standard',
+     label: 'Standard',
+     order: 1,
      price: 39.99,
      stripe_subscription_plan: 'standard-500MB-39-99'
                    })
 AccountType.create({
      data_limit: 100000000000,
-     name: 'Advance - 1 GB',
+     name: 'advanced',
+     label: 'Advanced',
+     order: 2,
      price: 79.99,
      stripe_subscription_plan: 'advance-1GB-79-99'
                    })
 AccountType.create({
      data_limit: 200000000000,
-     name: 'Professional - 2 GB',
+     name: 'professional',
+     label: 'Professional',
+     order: 3,
      price: 179.99,
      stripe_subscription_plan: 'professional-2GB-179-99'
                    })
